cd208e38aa8f2e636b13d2bb07af4dfb03b14049
timur kharisov <tkharisov7@gmail.com>
2025-07-06 19:32:37 +0200

    fix mail spelling

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -1,5 +1,5 @@
 # remove all animations
-# taken from https://apple.stackexchange.com/questions/14001/how-to-turn-off-all-animations-on-os-x
+# # taken from https://apple.stackexchange.com/questions/14001/how-to-turn-off-all-animations-on-os-x
 defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
 defaults write -g NSScrollAnimationEnabled -bool false
 defaults write -g NSWindowResizeTime -float 0.001
@@ -15,5 +15,5 @@ defaults write com.apple.dock springboard-show-duration -float 0
 defaults write com.apple.dock springboard-hide-duration -float 0
 defaults write com.apple.dock springboard-page-duration -float 0
 defaults write com.apple.finder DisableAllAnimations -bool true
-defaults write com.apple.Mail DisableSendAnimations -bool true
-defaults write com.apple.Mail DisableReplyAnimations -bool true
+defaults write com.apple.mail DisableSendAnimations -bool true
+defaults write com.apple.mail DisableReplyAnimations -bool true
